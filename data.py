f7cc06a4c2bfbb7b837ae411314e0b8cc95899ce
Matthew <124092023+MatthewRayUK@users.noreply.github.com>
2025-02-05 14:49:35 +0000

    Data

---
+++ data.py
@@ -0,0 +1,17 @@
+my_token = 'MTMzMjgzMDU0NTkwNTk3NTMxNw.Gfwv88.EFf6XMhw76Hi5kCYHH9b-11itV-zkPoDwR67QY'
+
+celebrations = [
+    "Keep crushing it, you're unstoppable!",
+    "Way to go, you're a coding rockstar!",
+    "One step closer to mastering Python!",
+    "You're on fire, keep the momentum going!",
+    "Every day is a victory, well done!",
+    "You've got this, the finish line is closer than ever!",
+    "You're making serious strides, keep it up!",
+    "Your dedication is inspiring!",
+    "Another day down, another win for you!",
+    "You’re on your way to greatness!",
+    "Keep the code flowing, you’re doing amazing!",
+    "You’re a coding machine, unstoppable!",
+    "Woohoooooo! Angela would be so proud!",
+]
